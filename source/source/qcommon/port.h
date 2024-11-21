9c7cbea7bdff3b898f75adc7d2c378aa8b9ce3d1
Alibek Omarov <a1ba.omarov@gmail.com>
2024-11-22 01:50:43 +0300

    qcommon: port: add GAME_EXPORT attribute. This must be set on all exported to the engine functions to keep it old, pre-SSE ABI compatible

--- source/source/qcommon/port.h
+++ source/source/qcommon/port.h
@@ -9,11 +9,16 @@
 #endif
 
 #ifdef __GNUC__
-	#define EXPORT __attribute__(( visibility( "default" )))
+	#ifdef __i386__
+		#define EXPORT      __attribute__(( visibility( "default" ), force_align_arg_pointer ))
+		#define GAME_EXPORT __attribute__(( force_align_arg_pointer ))
+	#else
+		#define EXPORT      __attribute__(( visibility( "default" )))
+		#define GAME_EXPORT
+	#endif
 #elif defined( _MSC_VER )
-	#define EXPORT __declspec( dllexport )
-#else
-	#define EXPORT
+	#define EXPORT      __declspec( dllexport )
+	#define GAME_EXPORT
 #endif
 
 #endif // PORT_H
